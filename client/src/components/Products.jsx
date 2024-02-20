c0007f685e959a6dee462579a530899f822db253
davidd74 <lolercoolol@gmail.com>
2024-02-20 19:44:28 +0100

    set up debugging environment for Minified React error #31

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -7,115 +7,126 @@ import toast from "react-hot-toast";
 import RatingFormula from "./RatingFormula";
 
 const Products = () => {
-  const { data, isLoading, error } = useGetProductsQuery();
-  const [loading, setLoading] = useState(true);
+  // const { data, isLoading, error } = useGetProductsQuery();
+  const data = [
+    {
+      price: 100,
+      rating: 4,
+      image: "nema slike bajo",
+    },
+  ];
+
+  const [loading, setLoading] = useState(false);
 
   console.log(data);
 
-  useEffect(() => {
-    setLoading(true);
-    if (data && data.length > 0) {
-      let imagesToLoad = data.length;
+  // useEffect(() => {
+  //   setLoading(true);
+  //   if (data && data.length > 0) {
+  //     let imagesToLoad = data.length;
 
-      const handleImageLoad = () => {
-        imagesToLoad--;
-        if (imagesToLoad === 0) {
-          setLoading(false);
-        }
-      };
+  //     const handleImageLoad = () => {
+  //       imagesToLoad--;
+  //       if (imagesToLoad === 0) {
+  //         setLoading(false);
+  //       }
+  //     };
 
-      data.forEach((product) => {
-        const imageElement = new Image();
-        imageElement.src = product.image.url;
-        imageElement.addEventListener("load", handleImageLoad);
+  //     data.forEach((product) => {
+  //       const imageElement = new Image();
+  //       imageElement.src = product.image.url;
+  //       imageElement.addEventListener("load", handleImageLoad);
 
-        return () => {
-          imageElement.removeEventListener("load", handleImageLoad);
-        };
-      });
-    }
-  }, [data]);
+  //       return () => {
+  //         imageElement.removeEventListener("load", handleImageLoad);
+  //       };
+  //     });
+  //   }
+  // }, [data]);
 
-  if (error) {
-    toast.error(error);
-  }
+  // if (error) {
+  //   toast.error(error);
+  // }
 
   return (
     <div>
-      {isLoading || loading ? (
+      {loading ? (
         <div className="flex items-center justify-center h-screen">
           <SyncLoader color="#58B1FF" />
         </div>
       ) : (
         <div>
           <Grid container spacing={2.5} marginTop={"3.5rem"}>
-            {data?.map((product) => {
-              const averageRating = product.reviews.reduce(
-                (acc, review) => acc + review.rating,
-                0,
-              );
+            {data &&
+              data?.map((product) => {
+                // const averageRating = product.reviews.reduce(
+                //   (acc, review) => acc + review.rating,
+                //   0,
+                // );
 
-              return (
-                <Grid item xs={6} md={3} lg={2} key={product._id}>
-                  <Card
-                    sx={{
-                      bgcolor: "#1E1E1E",
-                      height: "100%",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
-                      borderRadius: "20px",
-                    }}
-                  >
-                    <Link to={`/product/${product?._id}`}>
-                      <CardMedia
-                        component="img"
-                        image={product?.image.url}
-                        alt={product?.name}
-                      />
-                    </Link>
+                const averageRating = 3.5;
 
-                    <CardContent
+                return (
+                  <Grid item xs={6} md={3} lg={2} key={product._id}>
+                    <Card
                       sx={{
+                        bgcolor: "#1E1E1E",
                         height: "100%",
                         display: "flex",
                         flexDirection: "column",
                         justifyContent: "space-between",
+                        borderRadius: "20px",
                       }}
                     >
-                      <div>
-                        <p className="text-sm font-medium text-gray-400 duration-200 cursor-pointer transition-text hover:text-primary-600">
-                          {product?.brand}
-                        </p>
-                        <Link to={`/product/${product?._id}`}>
-                          <h3 className="text-lg font-medium text-white duration-200 cursor-pointer transition-text hover:text-primary-400">
-                            {product?.name}
-                          </h3>
-                        </Link>
-                      </div>
-                      <div>
-                        {averageRating ? (
-                          <div className="flex gap-2 pt-1">
-                            <RatingFormula rating={averageRating} />
-                            <p className="text-white">
-                              {averageRating.toFixed(1)}
-                            </p>
-                          </div>
-                        ) : (
-                          <div className="flex gap-2 pt-1">
-                            <RatingFormula rating={averageRating} />
-                            <p className="text-white">0</p>
-                          </div>
-                        )}
-                        <p className="mt-4 text-2xl font-medium text-slate-50">
-                          ${product?.price}
-                        </p>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              );
-            })}
+                      <Link to={`/product/${product?._id}`}>
+                        <CardMedia
+                          component="img"
+                          image={product?.image.url}
+                          alt={product?.name}
+                        />
+                      </Link>
+
+                      <CardContent
+                        sx={{
+                          height: "100%",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <div>
+                          <p className="text-sm font-medium text-gray-400 duration-200 cursor-pointer transition-text hover:text-primary-600">
+                            {product?.brand}
+                          </p>
+                          <Link to={`/product/${product?._id}`}>
+                            <h3 className="text-lg font-medium text-white duration-200 cursor-pointer transition-text hover:text-primary-400">
+                              {product?.name}
+                            </h3>
+                          </Link>
+                        </div>
+                        <div>
+                          {averageRating ? (
+                            <div className="flex gap-2 pt-1">
+                              <RatingFormula rating={averageRating} />
+                              <p className="text-white">
+                                {averageRating.toFixed(1)}
+                              </p>
+                            </div>
+                          ) : (
+                            <div className="flex gap-2 pt-1">
+                              <RatingFormula rating={averageRating} />
+                              <p className="text-white">0</p>
+                            </div>
+                          )}
+                          <p className="mt-4 text-2xl font-medium text-slate-50">
+                            ${product?.price}
+                          </p>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                );
+              })}
           </Grid>
         </div>
       )}
